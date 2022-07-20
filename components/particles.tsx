198a7c11d39d9adc138b00703693756483d164aa
danielx-art <metacarpo10@gmail.com>
2022-07-20 13:25:08 -0300

    particles and refresh button

---
+++ components/particles.tsx
@@ -0,0 +1,72 @@
+
+import * as THREE from 'three';
+import React, { useRef, useMemo, Ref } from 'react';
+import { useFrame } from '@react-three/fiber';
+
+
+const Particles = () => {
+
+    //should be passed as props and input by the user
+    const count = 300;
+
+    const mesh = useRef<THREE.InstancedMesh>(null);
+    const light = useRef<THREE.PointLight>(null);
+
+    const particles = useMemo(() => {
+        const temp = [];
+        for (let i = 0; i < count; i++) {
+          const time = Math.random()*100;
+          const factor = Math.random()*100 + 20;
+          const speed = (Math.random()*0.014 + 0.01)/ 2;
+          const x = Math.random()*100 - 50;
+          const y = Math.random()*100 - 50;
+          const z = Math.random()*100 - 50;
+      
+          temp.push({ time, factor, speed, x, y, z });
+        }
+        return temp;
+    }, [count]);
+
+    const dummy = useMemo(() => new THREE.Object3D(), []);
+
+    useFrame(() => {
+        // Run through the randomized data to calculate some movement
+        particles.forEach((particle, index) => {
+            let { factor, speed, x, y, z } = particle;
+
+            // Update the particle time
+            const t = (particle.time += speed);
+
+            // Update the particle position based on the time
+            // This is mostly random trigonometry functions to oscillate around the (x, y, z) point
+            dummy.position.set(
+                x + Math.cos((t / 10) * factor) + (Math.sin(t * 1) * factor) / 10,
+                y + Math.sin((t / 10) * factor) + (Math.cos(t * 2) * factor) / 10,
+                z + Math.cos((t / 10) * factor) + (Math.sin(t * 3) * factor) / 10
+            );
+
+            // Derive an oscillating value which will be used
+            // for the particle size and rotation
+            const s = Math.cos(t);
+            dummy.scale.set(s, s, s);
+            dummy.rotation.set(s * 5, s * 5, s * 5);
+            dummy.updateMatrix();
+
+            // And apply the matrix to the instanced item
+            if(mesh.current) mesh.current.setMatrixAt(index, dummy.matrix);
+        });
+        if(mesh.current) mesh.current.instanceMatrix.needsUpdate = true;
+    });
+
+    return (
+        <>
+        <pointLight ref={light} distance={40} intensity={3} color="lightblue" />
+            <instancedMesh ref={mesh} args={[, , count]}>
+                <dodecahedronBufferGeometry args={[0.2, 0]} />
+                <meshPhongMaterial color="#2596be" />
+            </instancedMesh>
+        </>
+    );
+}
+
+export default Particles
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
 import { Canvas } from '@react-three/fiber'
 import type { NextPage } from 'next'
-import Head from 'next/head'
+import Image from 'next/image'
+import Particles from '../components/particles'
 
 const Home: NextPage = () => {
   return (
     <>
-      <h1 className='text-3xl'>Hello world</h1>
-      <Canvas>
-        <mesh receiveShadow={true} castShadow={true}>
-        <boxBufferGeometry />
-        <meshPhysicalMaterial  color={"white"} />
-    </mesh>
+      <p className='text-1xl w-full text-center absolute'>Hit refresh</p>
+      <Canvas className='h-full w-full absolute'>
+        <Particles />
       </Canvas>
+      <div className='text-2xl w-full text-center z-10 sticky bottom-0 p-2'>
+        <button className='bg-black p-1 border border-white hover:rotate-90 active:opacity-50 transition-transform duration-700 ease-in-out rounded-full'>
+          <svg fill="white" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 32 32" width="2em" height="2em"><path d="M 16 4 C 10.886719 4 6.617188 7.160156 4.875 11.625 L 6.71875 12.375 C 8.175781 8.640625 11.710938 6 16 6 C 19.242188 6 22.132813 7.589844 23.9375 10 L 20 10 L 20 12 L 27 12 L 27 5 L 25 5 L 25 8.09375 C 22.808594 5.582031 19.570313 4 16 4 Z M 25.28125 19.625 C 23.824219 23.359375 20.289063 26 16 26 C 12.722656 26 9.84375 24.386719 8.03125 22 L 12 22 L 12 20 L 5 20 L 5 27 L 7 27 L 7 23.90625 C 9.1875 26.386719 12.394531 28 16 28 C 21.113281 28 25.382813 24.839844 27.125 20.375 Z"/></svg>
+        </button>
+      </div>
     </>
   )
 }
