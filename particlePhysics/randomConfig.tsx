1ca9573b9d5aacec9aa8de0e525b33f1773bf674
DanielX <metacarpo10@gmail.com>
2022-09-24 14:54:03 -0300

    test find generatorns name 5c

--- particlePhysics/randomConfig.tsx
+++ particlePhysics/randomConfig.tsx
@@ -20,7 +20,7 @@ import { Iparallelepiped, parallelepiped } from "./shapes";
 import vec from "./vetores";
 //import * as DISPLAY_GENERATORS from "../particlePhysics/generators/displayGenerators"
 
-type NamedFunction = Function & {name: string}
+interface NamedFunction extends Function {name: string}
 
 function pickRandomGenerator<NamedFunction>(importedGeneratorsObj: {
   [name: string]: NamedFunction;
